977e7e9f20d5c471393325d355eb0ed5d9d094e2
Navid Hamidi <nvdhamidi76@gmail.com>
2020-12-12 15:13:50 +0100

    Add mirror

---
+++ Assets/Scripts/Player/PlayerSetup.cs
@@ -0,0 +1,31 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class PlayerSetup : NetworkBehaviour
+{
+	// To avoid controlling all player 
+	[SerializeField] private Behaviour[] componentsToDisable;
+
+	GameObject mainCamera;
+
+	private void Start()
+	{
+		mainCamera = Camera.main.gameObject;
+
+		if (isLocalPlayer)
+		{
+			mainCamera.SetActive(false);
+			return;
+		}
+
+		for (int i = 0; i < componentsToDisable.Length; i++)
+			componentsToDisable[i].enabled = false;
+
+
+	}
+
+	private void OnDisable()
+	{
+		mainCamera.SetActive(true);
+	}
+}
