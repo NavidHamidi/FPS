24bdedf0c1fae486348871f0b8cb6980e56574d3
Navid Hamidi <nvdhamidi76@gmail.com>
2020-12-13 18:18:47 +0100

    add health script

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+	[SerializeField] private float maxHp;
+	[SerializeField] private float curHp;
+
+	private bool isAlive;
+
+	public void TakeDamage(float amount)
+	{
+		curHp -= amount;
+		if (curHp <= 0)
+			isAlive = false;
+	}
+
+	public void Heal(float amount)
+	{
+		curHp += amount;
+		if (curHp >= maxHp)
+			curHp = maxHp;
+	}
+
+	public void Die()
+	{
+		Debug.Log("Player die");
+	}
+
+	public void Start()
+	{
+		isAlive = true;
+	}
+
+	public void Update()
+	{
+		if (!isAlive)
+			Die();
+	}
+}
